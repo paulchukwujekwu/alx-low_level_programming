4a5ea727041d3c739bad577a3f3c30f95ad13773
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-08-10 20:38:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Return: will return 1 as success in this case
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
