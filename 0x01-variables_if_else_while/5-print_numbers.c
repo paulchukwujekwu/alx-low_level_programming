47d69da420c7300d79b1493d6f0eacfc7523b995
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-08-11 21:14:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * Description: program that prints the alphabet in lowercase
+ * Return: always return zero for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
