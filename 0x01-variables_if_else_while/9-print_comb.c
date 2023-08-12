2974df221b560838d3fd5b7e1f134b6ea5f64911
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-08-11 21:58:04 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * Description: program that prints the alphabet in lowercase
+ * Return: always return zero for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	if (i != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
