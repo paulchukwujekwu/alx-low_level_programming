1981219936fce24388eda25f24e0dd7041c4dc16
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-09-02 03:29:48 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main entrance to the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always return zero for success
+ **/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
