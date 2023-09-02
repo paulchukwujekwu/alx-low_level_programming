5bb3a53537247d8215c494ed8f3cdcefa83272a1
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2023-09-02 04:11:34 -0700

    C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - print number of argument passed to the program
  * @argc: argument count
  * @argv: argument vector
  * Return: returns zero
  **/
+
 int main(int argc, char *argv[])
 {
 	(void) argv;
 
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
+
 	return (0);
 }
